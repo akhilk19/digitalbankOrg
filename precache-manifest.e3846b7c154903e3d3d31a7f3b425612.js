c28505c5003a6b468b8c20be50c7982a771316d1
Akhilk19 <akhilk19@gmail.com>
2019-10-25 19:10:23 +0530

    Updates

---
+++ precache-manifest.e3846b7c154903e3d3d31a7f3b425612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f27b0c2f62d5ae45b55",
+    "url": "/digitalbankOrg/static/css/main.66c3516b.chunk.css"
+  },
+  {
+    "revision": "9f27b0c2f62d5ae45b55",
+    "url": "/digitalbankOrg/static/js/main.9f27b0c2.chunk.js"
+  },
+  {
+    "revision": "4e559ac82a8041e754b4",
+    "url": "/digitalbankOrg/static/css/1.9d6332e2.chunk.css"
+  },
+  {
+    "revision": "4e559ac82a8041e754b4",
+    "url": "/digitalbankOrg/static/js/1.4e559ac8.chunk.js"
+  },
+  {
+    "revision": "87bbe7c3b3379bbb6bc1",
+    "url": "/digitalbankOrg/static/js/runtime~main.87bbe7c3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/digitalbankOrg/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6f0a07c32a16e9a4e3d49eb74bcf73c7",
+    "url": "/digitalbankOrg/static/media/background.6f0a07c3.jpg"
+  },
+  {
+    "revision": "b89a21d3306363df14d18a8fdc6d0693",
+    "url": "/digitalbankOrg/index.html"
+  }
+];
