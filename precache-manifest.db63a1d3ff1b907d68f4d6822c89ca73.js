fe8cd9377a457bfe136959cc1c403edc1441b691
Akhilk19 <akhilk19@gmail.com>
2019-10-25 19:27:16 +0530

    Updates

---
+++ precache-manifest.db63a1d3ff1b907d68f4d6822c89ca73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "791cc5608ede2fed5c82",
+    "url": "./static/css/main.da85effb.chunk.css"
+  },
+  {
+    "revision": "791cc5608ede2fed5c82",
+    "url": "./static/js/main.791cc560.chunk.js"
+  },
+  {
+    "revision": "4e559ac82a8041e754b4",
+    "url": "./static/css/1.9d6332e2.chunk.css"
+  },
+  {
+    "revision": "4e559ac82a8041e754b4",
+    "url": "./static/js/1.4e559ac8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6f0a07c32a16e9a4e3d49eb74bcf73c7",
+    "url": "./static/media/background.6f0a07c3.jpg"
+  },
+  {
+    "revision": "7426e10538096c1f9816727ac19c1fa1",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/digitalbankOrg/precache-manifest.e3846b7c154903e3d3d31a7f3b425612.js"
+  "./precache-manifest.db63a1d3ff1b907d68f4d6822c89ca73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/digitalbankOrg/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
